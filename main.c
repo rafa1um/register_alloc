83b922f58096fd1e85ada7affc2f1af78c116c9a
rafa1um <rafael.rxp1@gmail.com>
2019-10-19 16:49:38 -0300

    Parsing

--- main.c
+++ main.c
@@ -1,28 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "modules/linked_list.h"
 
-int main() { 
-  /* Start with the empty list */
-  struct Node* head = NULL; 
-  
-  // Insert 6.  So linked list becomes 6->NULL 
-  append(&head, 6); 
-  
-  // Insert 7 at the beginning. So linked list becomes 7->6->NULL 
-  push(&head, 7); 
-  
-  // Insert 1 at the beginning. So linked list becomes 1->7->6->NULL 
-  push(&head, 1); 
-  
-  // Insert 4 at the end. So linked list becomes 1->7->6->4->NULL 
-  append(&head, 4); 
-  
-  // Insert 8, after 7. So linked list becomes 1->7->8->6->4->NULL 
-  insertAfter(head->next, 8); 
-  
-  printf("\n Created Linked list is: "); 
-  printList(head); 
-  
-  return 0; 
+int main(int argc, char *argv[]) { 
+ 
+    //char c;
+    char *token;
+    FILE *input_file = fopen(argv[1], "r");
+    char buffer[1024];
+    /* Start with the empty list */
+    struct Node* head = NULL; 
+    int graph_num;
+    int k_num;
+    int node;
+    int adj_node;
+    
+
+    if (input_file == NULL) { 
+        printf("Arquivo não pode ser aberto! \n"); 
+        exit(0); 
+    } 
+
+    // Read contents from file 
+    //c = fgetc(input_file); 
+
+    //while (c != EOF) { 
+      //printf ("%c", c); 
+      //c = fgetc(input_file); 
+    //} 
+
+    while (fgets(buffer, 1024, input_file) != NULL) { 
+        token = strtok(buffer, " =");
+        while(token != NULL){
+          if(strcmp(token, "Grafo") == 0) {
+            token = strtok(NULL, ":");
+            graph_num = atoi(token);
+            printf("%d\n", graph_num);
+            token = strtok(NULL, " ");
+          }
+          else if(strcmp(token, "K") == 0) {
+            token = strtok(NULL, " ");
+            k_num = atoi(token);
+            printf("%d\n", k_num);
+          } 
+          else if(strcmp(token, "-->") == 0) {
+            token = strtok(NULL, " ");
+            while(token) {
+              printf("%s\n", token);
+              token = strtok(NULL, " ");
+            }
+          }
+          else {
+            node = atoi(token);
+            append(&head, node);
+            printf("%d\n", node);
+          }
+          token = strtok(NULL, " =");
+        }
+    } 
+    printList(head);  
+    fclose(input_file); 
+ 
+  // printf("\n Created Linked list is: "); 
+  // printList(head); 
+
 } 
